293098a9fa0ff6d57174094ceb7dbca8cbb11a85
christopher550 <blstchris25@gmail.com>
2025-10-30 22:19:47 +0300

    m

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,45 +1,91 @@
+import "@testing-library/jest-dom"; 
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import '@testing-library/jest-dom';
-
 import App from "../App";
 
-// Pepperoni checkbox
-test("checkbox is initially unchecked", () => {
+test("pizza checkbox is initially unchecked", () => {
   render(<App />);
-
   const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
-
   expect(addPepperoni).not.toBeChecked();
+});
+
+test("toppings list initially contains only cheese", () => {
+  render(<App />);
+  expect(screen.getByText(/small cheese/i)).toBeInTheDocument(); // updated
 });
 
 test("checkbox appears as checked when user clicks it", () => {
   render(<App />);
-
   const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
-
   userEvent.click(addPepperoni);
   expect(addPepperoni).toBeChecked();
 });
 
-test("checkbox appears as unchecked when user clicks a second time", () => {
+test("topping appears in toppings list when checked", () => {
   render(<App />);
-
   const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
-
   userEvent.click(addPepperoni);
+  expect(screen.getByText(/small pepperoni/i)).toBeInTheDocument(); // updated
+});
 
-  expect(addPepperoni).toBeChecked();
-
-  userEvent.click(addPepperoni);
-
+test("selected topping disappears when checked a second time", () => {
+  render(<App />);
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+  userEvent.click(addPepperoni); // check
+  userEvent.click(addPepperoni); // uncheck
+  expect(screen.getByText(/small cheese/i)).toBeInTheDocument(); // updated
   expect(addPepperoni).not.toBeChecked();
 });
 
-// Size select element
+test("size select element initially displays 'Small'", () => {
+  render(<App />);
+  const selectSize = screen.getByLabelText(/select size/i);
+  expect(selectSize).toHaveDisplayValue("Small");
+});
 
-// "Your Selection" text
+test("select Size dropdown displays the user's selected value", () => {
+  render(<App />);
+  const selectSize = screen.getByLabelText(/select size/i);
+  userEvent.selectOptions(selectSize, "medium");
+  expect(selectSize).toHaveDisplayValue("Medium");
+  userEvent.selectOptions(selectSize, "large");
+  expect(selectSize).toHaveDisplayValue("Large");
+});
 
-// "Contact Info" text box
+test("'Your Selection' message initially displays 'small cheese'", () => {
+  render(<App />);
+  expect(screen.getByText(/small cheese/i)).toBeInTheDocument();
+});
 
-// Submit Order button
+test("selecting options updates the 'Your selection' message", () => {
+  render(<App />);
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+  const selectSize = screen.getByLabelText(/select size/i);
+  userEvent.click(addPepperoni);
+  expect(screen.getByText(/small pepperoni/i)).toBeInTheDocument();
+  userEvent.selectOptions(selectSize, "large");
+  expect(screen.getByText(/large pepperoni/i)).toBeInTheDocument();
+});
+
+test("'Contact Info' text box initially displays a placeholder value of 'email address'", () => {
+  render(<App />);
+  expect(screen.getByPlaceholderText(/email address/i)).toBeInTheDocument();
+});
+
+test("the page shows information the user types into the contact form field", () => {
+  render(<App />);
+  const contact = screen.getByLabelText(/enter your email address/i);
+  userEvent.type(contact, "pizzafan@email.com");
+  expect(contact).toHaveValue("pizzafan@email.com");
+});
+
+test("form contains a 'Submit Order' button", () => {
+  render(<App />);
+  expect(screen.getByRole("button", { name: /submit order/i })).toBeInTheDocument();
+});
+
+test("clicking the Place Order button displays a thank you message", () => {
+  render(<App />);
+  userEvent.click(screen.getByRole("button", { name: /submit order/i }));
+  expect(screen.getByText(/thanks for your order!/i)).toBeInTheDocument();
+});
